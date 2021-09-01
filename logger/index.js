e4c231f3df44b97fac0c92402e49800b17550e22
Roxenne Rosario <RRosario@Arcanys.com>
2021-09-01 17:11:11 +0800

    add winston for logging

---
+++ logger/index.js
@@ -0,0 +1,11 @@
+const winston = require('winston');
+ 
+const logger = winston.createLogger({
+  level: 'debug',
+  format: winston.format.simple(),
+  transports: [
+    new winston.transports.File({ filename: 'logs/error.log' }),
+  ],
+});
+
+module.exports = logger;
