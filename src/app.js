aadeead2b864d092d403d492be12e8a88c3724b1
rrosario-arcanys <89887479+rrosario-arcanys@users.noreply.github.com>
2021-09-02 13:26:32 +0800

    Revert "add tooling in test command, and fix lint issues"

--- src/app.js
+++ src/app.js
@@ -10,262 +10,225 @@ const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 
 const swaggerOptions = {
-  swaggerOptions: {
-    tryItOutEnabled: false,
-    supportedSubmitMethods: [''],
-  },
-  swaggerDefinition: {
-    info: {
-      version: '1.0.0',
-      title: 'Rides API',
-      description: 'This is a simple rides API project.',
-      contact: {
-        name: 'Roxenne Lourdes Rosario',
-      },
-      servers: ['http://localhost:8010'],
+    swaggerOptions: {
+        tryItOutEnabled: false,
+        supportedSubmitMethods: [''],
     },
-  },
-  apis: ['src/app.js'],
+    swaggerDefinition: {
+        info: {
+            version: "1.0.0",
+            title: "Rides API",
+            description: "This is a simple rides API project.",
+            contact: {
+                name: "Roxenne Lourdes Rosario"
+            },
+            servers: ["http://localhost:8010"]
+        }
+    },
+    apis: ["src/app.js"]
 };
 
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
-app.use(
-  '/api-docs',
-  swaggerUi.serve,
-  swaggerUi.setup(swaggerDocs, swaggerOptions),
-);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs, swaggerOptions));
 
 module.exports = (db) => {
-  /**
-   * @swagger
-   * /health:
-   *  get:
-   *      summary: Return 'Healthy' string response
-   *      responses:
-   *          '200':
-   *              description: A successful response
-   */
-  app.get('/health', (req, res) => res.send('Healthy'));
+    /**
+     * @swagger
+     * /health:
+     *  get:
+     *      summary: Return 'Healthy' string response
+     *      responses:
+     *          '200':
+     *              description: A successful response
+     */
+    app.get('/health', (req, res) => res.send('Healthy'));
 
-  /**
-   * @swagger
-   * /rides:
-   *  post:
-   *      summary: Add a new ride
-   *      consumes:
-   *          application/json:
-   *      parameters:
-   *        - in: body
-   *          name: ride
-   *          description: The ride to create
-   *          schema:
-   *              type: object
-   *              required:
-   *                  - startLatitude
-   *                  - startLongitude
-   *                  - endLatitude
-   *                  - endLongitude
-   *                  - riderName
-   *                  - driverName
-   *                  - driverVehicle
-   *              properties:
-   *                  startLatitude:
-   *                      type: integer
-   *                  startLongitude:
-   *                      type: integer
-   *                  endLatitude:
-   *                      type: integer
-   *                  endLongitude:
-   *                      type: integer
-   *                  riderName:
-   *                      type: string
-   *                  driverName:
-   *                      type: string
-   *                  driverVehicle:
-   *                      type: string
-   *      responses:
-   *          '200':
-   *              description: A successful response
-   *          'VALIDATION_ERROR':
-   *              description: Invalid input values
-   *          'SERVER_ERROR':
-   *              description: Unknown error
-   *
-   *
-   */
-  app.post('/rides', jsonParser, (req, res) => {
-    const startLatitude = Number(req.body.start_lat);
-    const startLongitude = Number(req.body.start_long);
-    const endLatitude = Number(req.body.end_lat);
-    const endLongitude = Number(req.body.end_long);
-    const riderName = req.body.rider_name;
-    const driverName = req.body.driver_name;
-    const driverVehicle = req.body.driver_vehicle;
+    
+    /**
+     * @swagger
+     * /rides:
+     *  post:
+     *      summary: Add a new ride
+     *      consumes:
+     *          application/json:
+     *      parameters:
+     *        - in: body
+     *          name: ride
+     *          description: The ride to create
+     *          schema:
+     *              type: object
+     *              required:
+     *                  - startLatitude
+     *                  - startLongitude
+     *                  - endLatitude
+     *                  - endLongitude
+     *                  - riderName
+     *                  - driverName
+     *                  - driverVehicle
+     *              properties:
+     *                  startLatitude:
+     *                      type: integer
+     *                  startLongitude:
+     *                      type: integer
+     *                  endLatitude:
+     *                      type: integer
+     *                  endLongitude:
+     *                      type: integer
+     *                  riderName:
+     *                      type: string
+     *                  driverName:
+     *                      type: string
+     *                  driverVehicle:
+     *                      type: string
+     *      responses:
+     *          '200':
+     *              description: A successful response
+     *          'VALIDATION_ERROR':
+     *              description: Invalid input values
+     *          'SERVER_ERROR':
+     *              description: Unknown error
+     *          
+     * 
+     */
+    app.post('/rides', jsonParser, (req, res) => {
+        const startLatitude = Number(req.body.start_lat);
+        const startLongitude = Number(req.body.start_long);
+        const endLatitude = Number(req.body.end_lat);
+        const endLongitude = Number(req.body.end_long);
+        const riderName = req.body.rider_name;
+        const driverName = req.body.driver_name;
+        const driverVehicle = req.body.driver_vehicle;
 
-    if (
-      startLatitude < -90 ||
-      startLatitude > 90 ||
-      startLongitude < -180 ||
-      startLongitude > 180
-    ) {
-      return res.send({
-        error_code: 'VALIDATION_ERROR',
-        message:
-          'Start latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively',
-      });
-    }
-
-    if (
-      endLatitude < -90 ||
-      endLatitude > 90 ||
-      endLongitude < -180 ||
-      endLongitude > 180
-    ) {
-      return res.send({
-        error_code: 'VALIDATION_ERROR',
-        message:
-          'End latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively',
-      });
-    }
-
-    if (typeof riderName !== 'string' || riderName.length < 1) {
-      return res.send({
-        error_code: 'VALIDATION_ERROR',
-        message: 'Rider name must be a non empty string',
-      });
-    }
-
-    if (typeof driverName !== 'string' || driverName.length < 1) {
-      return res.send({
-        error_code: 'VALIDATION_ERROR',
-        message: 'Rider name must be a non empty string',
-      });
-    }
-
-    if (
-      typeof driverVehicle !== 'string' ||
-      driverVehicle.length < 1
-    ) {
-      return res.send({
-        error_code: 'VALIDATION_ERROR',
-        message: 'Rider name must be a non empty string',
-      });
-    }
-
-    var values = [
-      req.body.start_lat,
-      req.body.start_long,
-      req.body.end_lat,
-      req.body.end_long,
-      req.body.rider_name,
-      req.body.driver_name,
-      req.body.driver_vehicle,
-    ];
-
-    db.run(
-      'INSERT INTO Rides(startLat, startLong, endLat, endLong, riderName, driverName, driverVehicle) VALUES (?, ?, ?, ?, ?, ?, ?)',
-      values,
-      function (err) {
-        if (err) {
-          return res.send({
-            error_code: 'SERVER_ERROR',
-            message: 'Unknown error',
-          });
+        if (startLatitude < -90 || startLatitude > 90 || startLongitude < -180 || startLongitude > 180) {
+            return res.send({
+                error_code: 'VALIDATION_ERROR',
+                message: 'Start latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively'
+            });
         }
 
-        db.all(
-          'SELECT * FROM Rides WHERE rideID = ?',
-          this.lastID,
-          function (err, rows) {
+        if (endLatitude < -90 || endLatitude > 90 || endLongitude < -180 || endLongitude > 180) {
+            return res.send({
+                error_code: 'VALIDATION_ERROR',
+                message: 'End latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively'
+            });
+        }
+
+        if (typeof riderName !== 'string' || riderName.length < 1) {
+            return res.send({
+                error_code: 'VALIDATION_ERROR',
+                message: 'Rider name must be a non empty string'
+            });
+        }
+
+        if (typeof driverName !== 'string' || driverName.length < 1) {
+            return res.send({
+                error_code: 'VALIDATION_ERROR',
+                message: 'Rider name must be a non empty string'
+            });
+        }
+
+        if (typeof driverVehicle !== 'string' || driverVehicle.length < 1) {
+            return res.send({
+                error_code: 'VALIDATION_ERROR',
+                message: 'Rider name must be a non empty string'
+            });
+        }
+
+        var values = [req.body.start_lat, req.body.start_long, req.body.end_lat, req.body.end_long, req.body.rider_name, req.body.driver_name, req.body.driver_vehicle];
+        
+        const result = db.run('INSERT INTO Rides(startLat, startLong, endLat, endLong, riderName, driverName, driverVehicle) VALUES (?, ?, ?, ?, ?, ?, ?)', values, function (err) {
             if (err) {
-              return res.send({
-                error_code: 'SERVER_ERROR',
-                message: 'Unknown error',
-              });
+                return res.send({
+                    error_code: 'SERVER_ERROR',
+                    message: 'Unknown error'
+                });
+            }
+
+            db.all('SELECT * FROM Rides WHERE rideID = ?', this.lastID, function (err, rows) {
+                if (err) {
+                    return res.send({
+                        error_code: 'SERVER_ERROR',
+                        message: 'Unknown error'
+                    });
+                }
+
+                res.send(rows);
+            });
+        });
+    });
+
+    /**
+     * @swagger
+     * /rides:
+     *  get:
+     *      summary: Get all rides
+     *      responses:
+     *          '200':
+     *              description: A successful response
+     *          'SERVER_ERROR':
+     *              description: Unknown error
+     *          'RIDES_NOT_FOUND_ERROR':
+     *              description: No rides found
+     */
+    app.get('/rides', (req, res) => {
+        db.all('SELECT * FROM Rides', function (err, rows) {
+            if (err) {
+                return res.send({
+                    error_code: 'SERVER_ERROR',
+                    message: 'Unknown error'
+                });
+            }
+
+            if (rows.length === 0) {
+                return res.send({
+                    error_code: 'RIDES_NOT_FOUND_ERROR',
+                    message: 'Could not find any rides'
+                });
             }
 
             res.send(rows);
-          },
-        );
-      },
-    );
-  });
-
-  /**
-   * @swagger
-   * /rides:
-   *  get:
-   *      summary: Get all rides
-   *      responses:
-   *          '200':
-   *              description: A successful response
-   *          'SERVER_ERROR':
-   *              description: Unknown error
-   *          'RIDES_NOT_FOUND_ERROR':
-   *              description: No rides found
-   */
-  app.get('/rides', (req, res) => {
-    db.all('SELECT * FROM Rides', function (err, rows) {
-      if (err) {
-        return res.send({
-          error_code: 'SERVER_ERROR',
-          message: 'Unknown error',
         });
-      }
-
-      if (rows.length === 0) {
-        return res.send({
-          error_code: 'RIDES_NOT_FOUND_ERROR',
-          message: 'Could not find any rides',
-        });
-      }
-
-      res.send(rows);
     });
-  });
 
-  /**
-   * @swagger
-   * /rides/{id}:
-   *  get:
-   *      summary: Get a ride by ID
-   *      parameters:
-   *        - in: path
-   *          name: id
-   *          schema:
-   *              type: integer
-   *          required: true
-   *          description: Numeric ID of the ride to get
-   *      responses:
-   *          '200':
-   *              description: A successful response
-   *          'SERVER_ERROR':
-   *              description: Unknown error
-   *          'RIDES_NOT_FOUND_ERROR':
-   *              description: No rides found
-   */
-  app.get('/rides/:id', (req, res) => {
-    db.all(
-      `SELECT * FROM Rides WHERE rideID='${req.params.id}'`,
-      function (err, rows) {
-        if (err) {
-          return res.send({
-            error_code: 'SERVER_ERROR',
-            message: 'Unknown error',
-          });
-        }
+    /**
+     * @swagger
+     * /rides/{id}:
+     *  get:
+     *      summary: Get a ride by ID
+     *      parameters:
+     *        - in: path
+     *          name: id
+     *          schema:
+     *              type: integer
+     *          required: true
+     *          description: Numeric ID of the ride to get
+     *      responses:
+     *          '200':
+     *              description: A successful response
+     *          'SERVER_ERROR':
+     *              description: Unknown error
+     *          'RIDES_NOT_FOUND_ERROR':
+     *              description: No rides found
+     */
+    app.get('/rides/:id', (req, res) => {
+        db.all(`SELECT * FROM Rides WHERE rideID='${req.params.id}'`, function (err, rows) {
+            if (err) {
+                return res.send({
+                    error_code: 'SERVER_ERROR',
+                    message: 'Unknown error'
+                });
+            }
 
-        if (rows.length === 0) {
-          return res.send({
-            error_code: 'RIDES_NOT_FOUND_ERROR',
-            message: 'Could not find any rides',
-          });
-        }
+            if (rows.length === 0) {
+                return res.send({
+                    error_code: 'RIDES_NOT_FOUND_ERROR',
+                    message: 'Could not find any rides'
+                });
+            }
 
-        res.send(rows);
-      },
-    );
-  });
+            res.send(rows);
+        });
+    });
 
-  return app;
+    return app;
 };
--- tests/api.test.js
+++ tests/api.test.js
@@ -1,8 +1,6 @@
 'use strict';
 
-const { text } = require('express');
 const request = require('supertest');
-const assert = require('assert');
 
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database(':memory:');
@@ -11,204 +9,24 @@ const app = require('../src/app')(db);
 const buildSchemas = require('../src/schemas');
 
 describe('API tests', () => {
-  let rides;
+    before((done) => {
+        db.serialize((err) => { 
+            if (err) {
+                return done(err);
+            }
 
-  before((done) => {
-    db.serialize((err) => {
-      if (err) {
-        return done(err);
-      }
+            buildSchemas(db);
 
-      buildSchemas(db);
-
-      done();
-    });
-  });
-
-  beforeEach(function () {
-    rides = {
-      start_lat: 0, //invalid
-      start_long: 0, //invalid
-      end_lat: 50,
-      end_long: 100,
-      rider_name: 'Rider Name',
-      driver_name: 'Driver Name',
-      driver_vehicle: 'Vehicle Name',
-    };
-  });
-
-  describe('GET /health', () => {
-    it('should return health', (done) => {
-      request(app)
-        .get('/health')
-        .expect('Content-Type', /text/)
-        .expect(200, done);
-    });
-  });
-
-  describe('POST /rides', () => {
-    it('should add a ride successfully', (done) => {
-      request(app).post('/rides').send(rides).expect(200, done);
-    });
-
-    it('should return validation error when start latitude and longitude values are invalid', (done) => {
-      rides.start_lat = 100;
-      rides.start_long = 200;
-
-      const expected_response = {
-        error_code: 'VALIDATION_ERROR',
-        message:
-          'Start latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively',
-      };
-
-      request(app)
-        .post('/rides')
-        .send(rides)
-        .expect(200)
-        .end(function (err, res) {
-          assert.deepEqual(res.body, expected_response);
-          return done();
+            done();
         });
     });
 
-    it('should return validation error when start longitude values are invalid', (done) => {
-      rides.end_lat = 100;
-      rides.end_long = 200;
-
-      const expected_response = {
-        error_code: 'VALIDATION_ERROR',
-        message:
-          'End latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively',
-      };
-
-      request(app)
-        .post('/rides')
-        .send(rides)
-        .expect(200)
-        .end(function (err, res) {
-          assert.deepEqual(res.body, expected_response);
-          return done();
+    describe('GET /health', () => {
+        it('should return health', (done) => {
+            request(app)
+                .get('/health')
+                .expect('Content-Type', /text/)
+                .expect(200, done);
         });
     });
-
-    it('should return validation error when driver name is empty string', (done) => {
-      rides.driver_name = '';
-
-      const expected_response = {
-        error_code: 'VALIDATION_ERROR',
-        message: 'Rider name must be a non empty string',
-      };
-
-      request(app)
-        .post('/rides')
-        .send(rides)
-        .expect(200)
-        .end(function (err, res) {
-          assert.deepEqual(res.body, expected_response);
-          return done();
-        });
-    });
-
-    it('should return validation error when driver name is not a string', (done) => {
-      rides.driver_name = 123;
-
-      const expected_response = {
-        error_code: 'VALIDATION_ERROR',
-        message: 'Rider name must be a non empty string',
-      };
-
-      request(app)
-        .post('/rides')
-        .send(rides)
-        .expect(200)
-        .end(function (err, res) {
-          assert.deepEqual(res.body, expected_response);
-          return done();
-        });
-    });
-
-    it('should return validation error when rider name is empty string', (done) => {
-      rides.rider_name = '';
-
-      const expected_response = {
-        error_code: 'VALIDATION_ERROR',
-        message: 'Rider name must be a non empty string',
-      };
-
-      request(app)
-        .post('/rides')
-        .send(rides)
-        .expect(200)
-        .end(function (err, res) {
-          assert.deepEqual(res.body, expected_response);
-          return done();
-        });
-    });
-
-    it('should return validation error when rider name is not a string', (done) => {
-      rides.rider_name = 123;
-
-      const expected_response = {
-        error_code: 'VALIDATION_ERROR',
-        message: 'Rider name must be a non empty string',
-      };
-
-      request(app)
-        .post('/rides')
-        .send(rides)
-        .expect(200)
-        .end(function (err, res) {
-          assert.deepEqual(res.body, expected_response);
-          return done();
-        });
-    });
-
-    it('should return validation error when driver vehicle is empty string', (done) => {
-      rides.driver_vehicle = '';
-
-      const expected_response = {
-        error_code: 'VALIDATION_ERROR',
-        message: 'Rider name must be a non empty string',
-      };
-
-      request(app)
-        .post('/rides')
-        .send(rides)
-        .expect(200)
-        .end(function (err, res) {
-          assert.deepEqual(res.body, expected_response);
-          return done();
-        });
-    });
-
-    it('should return validation error when driver vehicle is not a string', (done) => {
-      rides.driver_vehicle = 123;
-
-      const expected_response = {
-        error_code: 'VALIDATION_ERROR',
-        message: 'Rider name must be a non empty string',
-      };
-
-      request(app)
-        .post('/rides')
-        .send(rides)
-        .expect(200)
-        .end(function (err, res) {
-          assert.deepEqual(res.body, expected_response);
-          return done();
-        });
-    });
-  });
-
-  describe('GET /rides', () => {
-    it('should return all rides', (done) => {
-      request(app).get('/rides').expect(200, done);
-    });
-  });
-
-  describe('GET /rides/{id}', () => {
-    it('should return ride given the id', (done) => {
-      request(app).get('/rides/1').expect(200, done);
-    });
-  });
 });
